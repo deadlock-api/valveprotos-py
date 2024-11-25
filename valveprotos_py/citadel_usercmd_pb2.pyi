15a070b489d1f6b21f5f1103af7c3c1a053d72f8
Manuel Raimann <raimannma@outlook.de>
2024-11-25 19:36:56 +0100

    Update

--- valveprotos_py/citadel_usercmd_pb2.pyi
+++ valveprotos_py/citadel_usercmd_pb2.pyi
@@ -8,25 +8,25 @@ from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Map
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class CCitadelUserCmdPB(_message.Message):
-    __slots__ = ["ang_camera_angles", "base", "camera_roaming_speed", "enemy_hero_aimed_at", "execute_ability_indices", "in_shop", "mouse_delta_x", "mouse_delta_y", "using_free_cursor", "vec_camera_position"]
+    __slots__ = ["ang_camera_angles", "base", "camera_roaming_speed", "enemy_hero_aimed_at", "execute_ability_indices", "in_shop", "using_free_cursor", "vec_camera_position", "view_delta_x", "view_delta_y"]
     ANG_CAMERA_ANGLES_FIELD_NUMBER: _ClassVar[int]
     BASE_FIELD_NUMBER: _ClassVar[int]
     CAMERA_ROAMING_SPEED_FIELD_NUMBER: _ClassVar[int]
     ENEMY_HERO_AIMED_AT_FIELD_NUMBER: _ClassVar[int]
     EXECUTE_ABILITY_INDICES_FIELD_NUMBER: _ClassVar[int]
     IN_SHOP_FIELD_NUMBER: _ClassVar[int]
-    MOUSE_DELTA_X_FIELD_NUMBER: _ClassVar[int]
-    MOUSE_DELTA_Y_FIELD_NUMBER: _ClassVar[int]
     USING_FREE_CURSOR_FIELD_NUMBER: _ClassVar[int]
     VEC_CAMERA_POSITION_FIELD_NUMBER: _ClassVar[int]
+    VIEW_DELTA_X_FIELD_NUMBER: _ClassVar[int]
+    VIEW_DELTA_Y_FIELD_NUMBER: _ClassVar[int]
     ang_camera_angles: _networkbasetypes_pb2.CMsgQAngle
     base: _usercmd_pb2.CBaseUserCmdPB
     camera_roaming_speed: float
     enemy_hero_aimed_at: int
     execute_ability_indices: int
     in_shop: bool
-    mouse_delta_x: _containers.RepeatedScalarFieldContainer[int]
-    mouse_delta_y: _containers.RepeatedScalarFieldContainer[int]
     using_free_cursor: bool
     vec_camera_position: _networkbasetypes_pb2.CMsgVector
-    def __init__(self, base: _Optional[_Union[_usercmd_pb2.CBaseUserCmdPB, _Mapping]] = ..., vec_camera_position: _Optional[_Union[_networkbasetypes_pb2.CMsgVector, _Mapping]] = ..., ang_camera_angles: _Optional[_Union[_networkbasetypes_pb2.CMsgQAngle, _Mapping]] = ..., execute_ability_indices: _Optional[int] = ..., in_shop: bool = ..., camera_roaming_speed: _Optional[float] = ..., using_free_cursor: bool = ..., enemy_hero_aimed_at: _Optional[int] = ..., mouse_delta_x: _Optional[_Iterable[int]] = ..., mouse_delta_y: _Optional[_Iterable[int]] = ...) -> None: ...
+    view_delta_x: _containers.RepeatedScalarFieldContainer[int]
+    view_delta_y: _containers.RepeatedScalarFieldContainer[int]
+    def __init__(self, base: _Optional[_Union[_usercmd_pb2.CBaseUserCmdPB, _Mapping]] = ..., vec_camera_position: _Optional[_Union[_networkbasetypes_pb2.CMsgVector, _Mapping]] = ..., ang_camera_angles: _Optional[_Union[_networkbasetypes_pb2.CMsgQAngle, _Mapping]] = ..., execute_ability_indices: _Optional[int] = ..., in_shop: bool = ..., camera_roaming_speed: _Optional[float] = ..., using_free_cursor: bool = ..., enemy_hero_aimed_at: _Optional[int] = ..., view_delta_x: _Optional[_Iterable[int]] = ..., view_delta_y: _Optional[_Iterable[int]] = ...) -> None: ...
